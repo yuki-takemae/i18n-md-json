58db2abddd274f4aee96718c1e6b9b031d5d3bab
Takemae, Yuki 竹前 勇貴 <takemae.yuki@jp.fujitsu.com>
2024-06-03 19:11:57 +0900

    build

--- vite.config.mjs
+++ vite.config.mjs
@@ -10,6 +10,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'i18n-md-json', //追加
   plugins: [
     Vue({
       template: { transformAssetUrls }
